46b2d6a6ae2f572b0de58d1667e68b8291ca6d0c
Lewis Barnes <lb.barnes@live.co.uk>
2015-11-28 15:18:23 +0000

    Made use of addEventListener. Errors now propagates to the callback. - Removed the need to add the video element to the DOM (I've no idea I had to do this).

--- video-thumbnail/public/main.js
+++ video-thumbnail/public/main.js
@@ -2,52 +2,51 @@ function generateThumbnail(src, type, callback) {
     var $video = document.createElement("video");
     var $canvas = document.createElement("canvas");
     var context = $canvas.getContext("2d");
-    var duration;
+    var width, height;
 
-    $video.crossOrigin = "anonymous";
-    $video.src = src;
-    $video.muted = true;
-
-    $video.onloadedmetadata = function() {
-        // Seek to the middle of the video.
+    $video.addEventListener("loadedmetadata", function() {
+         // Seek to the middle of the video.
         this.currentTime = this.duration / 2;
-    };
-    
-    $video.oncanplay = function() {
-        var width = this.videoWidth / 2;
-        var height = this.videoHeight / 2;
-        
-        this.play();
+
+        width = this.videoWidth / 2;
+        height = this.videoHeight / 2;
 
         $canvas.width = width;
         $canvas.height = height;
-        
+    }, true);
+
+    $video.addEventListener("canplay", function() {
         context.drawImage(this, 0, 0, width, height);
         this.pause();
 
-        // TODO: Find a way to remove this.
-        document.body.appendChild($video);
-
         callback(null, $canvas.toDataURL());
-    };
+    }, true);
 
-    $video.error = function(e) {
-        callback(e);
-    };
+    $video.addEventListener("error", callback, true);
+
+    $video.crossOrigin = "anonymous";
+    $video.preload = "auto";
+    $video.autoplay = true;
+    $video.muted = true;
+    $video.src = src;
+    $video.play();
 }
 
 // This will work running with Express.
 var videoURL = "video.mp4";
 
-// This won't work unless it has the "Access-Control-Allow-Origin: *" header set.
+// This won't work unless it has the "Access-Control-Allow-Origin: *" header se.tfa
 // var videoURL = "http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4";
 
 generateThumbnail(videoURL, "video/mp4", function(err, imageData) {
     if (err)
         throw err;
-
+    
     // Will log the base64 encoded string in console.
-    console.log(imageData);
+    console.log("imageData:", imageData);
+    
+    var $img = document.getElementById("thumbnail");
+    $img.src = imageData;
 });
 
 
