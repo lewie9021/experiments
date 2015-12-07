f9679098c816585a11bf4ba0fdd0e58bcb20ddfb
Lewis Barnes <lb.barnes@live.co.uk>
2015-12-07 21:47:25 +0000

    The app can now add new todos and store history of each mutation. - The slider allows you to scroll back in time :)

--- todo-list-undo/main.js
+++ todo-list-undo/main.js
@@ -1,4 +1,4 @@
-function createRender() {
+function createRenderer() {
     var cache = {};
 
     return function render(name, context) {
@@ -17,13 +17,55 @@ function createRender() {
     };
 }
 
-var render = createRender();
+var render = createRenderer();
+
+var TODOS = Immutable.List([]);
+var HISTORY = [TODOS];
 
 var $app = document.getElementById("app");
+
 $app.innerHTML = render("app", {
-    title: "Example Todo Application",
-    todos: [
-        {todo: "Hello", done: true},
-        {todo: "World", done: false}
-    ]
+    title: "Example Todo Application"
 });
+
+var $historySlider = document.getElementById("history-slider");
+var $newTodo = document.getElementById("new-todo");
+var $addTodo = document.getElementById("add-todo");
+var $todos = document.getElementById("todos");
+
+function renderTodos() {
+    $todos.innerHTML = render("todos", {
+        todos: TODOS.toJS()
+    });
+}
+
+$historySlider.addEventListener("input", function(e) {
+    TODOS = HISTORY[e.target.value];
+    
+    renderTodos();
+});
+
+$addTodo.addEventListener("click", function() {
+    var value = $newTodo.value;
+
+    if (!value.length)
+        return;
+
+    // Add the new todo.
+    TODOS = TODOS.push(Immutable.Map({todo: value, done: false}));
+
+    // Append the new state to the history array.
+    HISTORY.push(TODOS);
+
+    // Increment the length of the slider.
+    $historySlider.setAttribute("max", HISTORY.length - 1);
+    $historySlider.value = HISTORY.length - 1;
+    
+    renderTodos();
+
+    // Clear the input
+    $newTodo.value = "";
+});
+
+
+
